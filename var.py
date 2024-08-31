5c114d9c8136e37daac04654a4d4333afd3784cc
Cenzo <162982407+CenzoGod@users.noreply.github.com>
2024-08-31 14:54:58 +0530

    Update var.py

--- var.py
+++ var.py
@@ -12,10 +12,10 @@ def make_int(str_input):
     return int_list
 
 class Var:
-    API_ID = int(os.getenv("API_ID", "12227067"))
-    API_HASH = os.getenv("API_HASH", "b463bedd791aa733ae2297e6520302fe")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "5850629523:AAEJBWduZfIGyERyiXA9ctu-E0RrcnDnoi0")
-    sudo = os.getenv("SUDO","6246163989 1817166549 6255094965")
+    API_ID = int(os.getenv("API_ID", "28255147"))
+    API_HASH = os.getenv("API_HASH", "8113960fe67c0cc815e6acce2aefb410")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7382235042:AAFv5nrAHJEnq3cuJUOTCGLKYdVDeIaYZnE")
+    sudo = os.getenv("SUDO","6663845789 6698364560")
     SUDO = []
     if sudo:
         SUDO = make_int(sudo)
